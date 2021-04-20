6ffd392dea1a111790ebbb87622ba4b5597680c9
Devil <76868561+lucifeermorningstar@users.noreply.github.com>
2021-04-20 15:42:10 +0530

    Update dyrogram.py

--- StringGen/dyrogram.py
+++ StringGen/dyrogram.py
@@ -1,15 +1,12 @@
-# © Copyright 2021 - 2022 TeamDaisyX
-# All Credit For This File Goes to Team DaisyX. 
-# Ported Here By Devil. 
+from config import ( BOT_TOKEN, API_ID, API_HASH ) 
 
+from pyrogram import Client
+from pyromod import listen
 
-from pyrogram import Client, errors
-
-from StringGen.config import get_str_key, get_int_key
-
-TOKEN = get_str_key("TOKEN", required=True)
-APP_ID = get_int_key("APP_ID", required=True)
-APP_HASH = get_str_key("APP_HASH", required=True)
-session_name = TOKEN.split(':')[0]
-pbot = Client(session_name, api_id=APP_ID, api_hash=APP_HASH, bot_token=TOKEN)
+app = Client(
+    "wbb",
+    bot_token=BOT_TOKEN,
+    api_id=API_ID,
+    api_hash=API_HASH
+)
 
