ca40eec2d6ab4ab941831c2169f44f11a5459192
Devil <76868561+lucifeermorningstar@users.noreply.github.com>
2021-04-20 16:09:21 +0530

    Update config.py

--- StringGen/config.py
+++ StringGen/config.py
@@ -1,1 +1,13 @@
+# © Copyright Devil ( @lucifeermorningstar ) 
+# Made By Devil ❤
+
+import os
+
+# Change Accordingly While Deploying To A VPS
+
+APP_ID = int(os.environ.get("APP_ID"))
+
+API_HASH = os.environ.get("API_HASH")
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
