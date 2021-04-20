569908369d74e540401190179e3504aa95117c89
Devil <76868561+lucifeermorningstar@users.noreply.github.com>
2021-04-20 17:24:14 +0530

    Update config.py

--- StringGen/config.py
+++ StringGen/config.py
@@ -11,5 +11,7 @@ API_HASH = os.environ.get("API_HASH")
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
+BOT_NAME = os.environ.get("BOT_NAME") 
+
 COMMAND_PREFIXES = os.environ.get("COMMAND_PREFIXES") 
 
