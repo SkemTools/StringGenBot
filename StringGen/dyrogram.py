0d6e7094e14f5441c5abb209844894de0ffdea2b
Devil <76868561+lucifeermorningstar@users.noreply.github.com>
2021-04-20 19:58:19 +0530

    Update dyrogram.py

--- StringGen/dyrogram.py
+++ StringGen/dyrogram.py
@@ -1,11 +1,11 @@
-from StringGen.config import ( BOT_TOKEN, API_ID, API_HASH ) 
+from StringGen.config import ( BOT_TOKEN, APP_ID, API_HASH ) 
 
 from pyrogram import Client
 from pyromod import listen
 
 devil = Client(
     bot_token=BOT_TOKEN,
-    api_id=API_ID,
+    api_id=APP_ID,
     api_hash=API_HASH
 )
 
